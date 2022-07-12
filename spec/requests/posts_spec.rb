4053944ea94769f321a2f0776c1e6ccea070108a
Georges Stefan <stefangeorges4799@gmail.com>
2022-07-12 17:31:02 -0300

    Updates swagger docs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -68,6 +68,26 @@ RSpec.describe 'posts', type: :request do
     end
   end
 
+  path '/users/{id}/posts' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('user_posts post') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
   path '/{id}/reply' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
