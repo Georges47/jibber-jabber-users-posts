1220e50328fb7e369910ee6d433fbd032acbfd79
Georges Stefan <stefangeorges4799@gmail.com>
2022-07-14 20:41:13 -0300

    Fixes cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://jibber-jabber.brazilsouth.cloudapp.azure.com',
-            'jibber-jabber.brazilsouth.cloudapp.azure.com',
-            '*'
+    origins 'http://jibber-jabber.brazilsouth.cloudapp.azure.com'
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: true
   end
 end
