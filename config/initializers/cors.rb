1624322836737a9d9eba00882e221a930546f7ef
Georges Stefan <stefangeorges4799@gmail.com>
2022-07-15 00:45:51 -0300

    Adds https to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://jibber-jabber.brazilsouth.cloudapp.azure.com'
+    origins 'https://jibber-jabber.brazilsouth.cloudapp.azure.com',
+            'http://jibber-jabber.brazilsouth.cloudapp.azure.com'
 
     resource "*",
       headers: :any,
